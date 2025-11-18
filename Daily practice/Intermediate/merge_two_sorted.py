a7d0aca6e989e443e76da7380d912f36a3bee07c
hrithick03 <hrithick0330k@gmail.com>
2025-11-18 20:55:21 +0530

    code practice day 22

---
+++ Daily practice/Intermediate/merge_two_sorted.py
@@ -0,0 +1,23 @@
+def merge_sorted_lists(list1, list2):
+    i, j = 0, 0
+    merged = []
+
+    # Traverse both lists
+    while i < len(list1) and j < len(list2):
+        if list1[i] <= list2[j]:
+            merged.append(list1[i])
+            i += 1
+        else:
+            merged.append(list2[j])
+            j += 1
+
+    # Add remaining elements
+    merged.extend(list1[i:])
+    merged.extend(list2[j:])
+
+    return merged
+
+# Example
+list1 = [1, 3, 5, 7]
+list2 = [2, 4, 6, 8, 10]
+print(merge_sorted_lists(list1, list2))
